6064b607e65a3d5165cfde10cd6c5cf50ce1f482
Chun <Vivy1234423@gmail.com>
2022-11-30 22:23:48 +0200

    fixed bugs

--- main.py
+++ main.py
@@ -5,15 +5,25 @@ from csv import reader
 import json
 import sqlite3
 
-#still have to fix some bugs
+#getting list of top offers to exclude them from the search to fix issues with data
+
+url_top = f"https://balkanauction.com/bg/top-in-category/29"
+page_top = requests.get(url_top)
+soup_top = BeautifulSoup(page_top.content, 'lxml')
+
+lists_top = soup_top.find_all('a', class_="item")
+top_offers = []
+
+for list_top in lists_top:
+    title = list_top.find("div", class_="title").text
+    top_offers.append(title)
 
 def getInfoCSV(page, filename, count, cat, sort_type):
     url = f"https://balkanauction.com/bg/category/{cat}/{sort_type}&offset={page}"
-
     page = requests.get(url)
-
     soup = BeautifulSoup(page.content, 'lxml')
     lists = soup.find_all('a', class_="item")
+
 
     with open(f"{filename}", 'a', newline='') as f:
         thewriter = writer(f)
@@ -21,6 +31,7 @@ def getInfoCSV(page, filename, count, cat, sort_type):
         if count == 0:
             thewriter.writerow(header)
         for list in lists:
+
             title = list.find("div", class_="title").text
             price_int = list.find("span", class_="int-part").text
             price_frac = list.find("sup", class_="frac-part").text
@@ -30,9 +41,9 @@ def getInfoCSV(page, filename, count, cat, sort_type):
             seller = list.find("span", class_="username").text
             tag = list['href']
             link = "https://balkanauction.com/" + str(tag)
-
-            info = [title, price, remaining_time.strip(), seller, link]
-            thewriter.writerow(info)
+            if title not in top_offers:
+                info = [title, price, remaining_time.strip(), seller, link]
+                thewriter.writerow(info)
 
 
 def getInfoJSON(page, cat, sort_type):
@@ -63,7 +74,8 @@ def getInfoJSON(page, cat, sort_type):
             "Seller": seller,
             "Link": link
         }
-        jsonList.append(info)
+        if title not in top_offers:
+            jsonList.append(info)
     return jsonList
 
 
@@ -94,17 +106,13 @@ def getSQL(fname):
                 VALUES (?,?,?,?,?)''', (title, price, remaining_time, seller, link))
             conn.commit()
 
-
-theAnswer = input("Would you like to make csv , json file or sql?: ").lower()
-
-
 def cat_func():
     try:
         categories = """
-             1.stenni-chasovnitsi
-             2.dzhobni-chasovnitsi
-             3.nastolni-chasovnitsi
-             4.ruchni-chasovnitsi
+             1.Wall Clocks
+             2.Pocket Watches
+             3.Desktop Clocks
+             4.Wrist watches
         """
         print(categories)
         user_cat = int(input("Choose category: "))
@@ -130,10 +138,10 @@ def cat_func():
 def sort_func():
     try:
         sorting = """
-             1.Nai-evtini
-             2.Nai-skupi
-             3.Nai-novi
-             4.Zavurshvashti
+             1.Cheapest
+             2.The most expensive
+             3.New
+             4.Ending soon
         """
         print(sorting)
         user_sort = int(input("Sort by: "))
@@ -155,6 +163,20 @@ def sort_func():
     except:
         sort_func()
 
+resultsToScrape = 60
+pages = 0
+theAnswer = ""
+answers = ["csv","json","sql"]
+
+while theAnswer not in answers:
+    theAnswer = input("Would you like to make csv , xml, json file or sql?: ").lower()
+
+if theAnswer == "csv" or theAnswer == "json":
+    while pages <= 0:
+        pages = int(input("How many pages do you want to scrape: "))
+
+    pages = (resultsToScrape * pages) - 1
+
 
 if theAnswer == "csv":
     cat = cat_func()
@@ -163,7 +185,7 @@ if theAnswer == "csv":
     filename = str(userInput) + ".csv"
     count = 0
 
-    for i in range(0, 121, 60):
+    for i in range(0, pages, 60):
         getInfoCSV(i, filename, count, cat, sort_type)
         count += 1
 elif theAnswer == "json":
@@ -172,10 +194,15 @@ elif theAnswer == "json":
     userInput = input("Enter the name of your file: ")
     filename = str(userInput) + ".json"
     finalList = []
-    for i in range(0, 121, 60):
+    for i in range(0, pages, 60):
         finalList.append(getInfoJSON(i, cat, sort_type))
     with open(f"{filename}", 'a', newline='') as f:
         json.dump(finalList, f, indent=4, ensure_ascii=False)
 elif theAnswer == "sql":
-    userInput = input("Enter the name of your file: ")
-    getSQL(userInput)
+    try:
+        userInput = input("Enter the name of your csv file: ")
+        getSQL(userInput)
+    except FileNotFoundError:
+        print("File doesn't exist!")
+    except:
+        print("Invalid input!")
